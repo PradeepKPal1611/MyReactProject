f8fffefa06c5fc9ecf522919b37c7c95334db618
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-10-09 12:15:08 +0530

    My First React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+// function App() {
+//   const [data, setData] = useState(null)
+//   const [print, setPrint] = useState(false)
+
+//   function GetData(val)
+//   {
+//     console.log(val.target.Value)
+//     setData(val.target.value)
+//     setPrint(false)
+//   }
+//   return (
+//     <div className="App">
+       
+//       {
+//        print?
+//         <h2>{data}</h2>
+//         :null
+//       }
+
+//       <input type="text" placeholder="Enter Your Value Here!" onChange={GetData}/><br/>
+//       <button onClick={()=>setPrint(true)}>Get Your Data</button>
+    
+//     </div>
+//   );
+// }
 
 function App() {
+  const[status,setStatus] = React.useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='App'>
+  {
+    status? <h1>Hello Guys Welcome!</h1>:null
+  }
+  <button onClick={()=>setStatus(false)}>Hide </button>
+  <button onClick={()=>setStatus(true)}>Show </button>
+  
+</div>
+);
+
 }
 
 export default App;
